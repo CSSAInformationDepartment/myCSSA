15e355fc56ceb9cc16683ff0f0c08ab104eefe90
Josh Lu <joshlu@Joshs-iMac.local>
2018-11-17 07:52:37 +1100

    edit mail serializer

--- myCSSA/adminHub/serializers.py
+++ myCSSA/adminHub/serializers.py
@@ -2,4 +2,12 @@ from rest_framework import serializers
 from adminHub import models as AdminModels
 
 class MessageListSerializers(serializers.ModelSerializer):
-    fields = ('messageId', 'repliedId', 'messageTitle', 'timeOfSend', '')
+    class Meta:
+        fields = ('messageId', 'repliedId', 'messageTitle', 'timeOfSend', 'isRead',
+        'isDraft','sender','receiver')
+        read_only_fields = ('messageId', 'repliedId')
+
+
+class  MessageBodySerializers(serializers.ModelSerializer):
+    class Meta:
+        fields = '__all__'
