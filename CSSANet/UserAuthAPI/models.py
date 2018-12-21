a72d04a16e8fe42afbe6fca19583bcc0c81ca37d
ShepherdMOZ <joshlubox@gmail.com>
2018-12-21 21:20:34 +1100

    new models for Blog API and Site Settings

--- CSSANet/UserAuthAPI/models.py
+++ CSSANet/UserAuthAPI/models.py
@@ -160,7 +160,7 @@ class User(AbstractUser):
 
 #用户信息主体 （继承自标准admin model，参照： https://www.zmrenwu.com/post/31/）
 class UserProfile (models.Model):
-    user = models.OneToOneField(User,on_delete=models.DO_NOTHING)
+    user = models.OneToOneField(User,on_delete=models.DO_NOTHING, primary_key=True, editable=False)
 
     avatar = models.ImageField(verbose_name="头像", upload_to=_GetUserDir,
     height_field=None, width_field=None, max_length=None,null=True, blank=True)
