bd975da23b810d38e0f4b0bfc8e65f0ee71be224
Howie4PP <shenhai0910@gmail.com>
2018-12-22 18:13:43 +1100

    adding invalid page

--- CSSANet/myCSSAhub/urls.py
+++ CSSANet/myCSSAhub/urls.py
@@ -11,5 +11,20 @@ urlpatterns = [
     path('login/', Views.login_page, name="hub_login"),
     path('logout/', Views.logout_page , name='hub_logout'),
     path('register/', Views.register_guide , name='hub_reg'),
-    path('regform/', Views.register_form , name='hub_regform'),
+    path('regform/', Views.register_form , name='hub_regform')
 ]
+
+
+################################# errors pages testing########################################
+# urlpatterns = [
+#     path('400/', Views.bad_request, name='bad_request'),
+#     path('403/', Views.permission_denied, name='permission_denied'),
+#     path('404/', Views.page_not_found, name='page_not_found'),
+#     path('500/', Views.server_error, name='server_error')
+# ]     
+################################# errors pages testing########################################
+
+handler400 = Views.bad_request
+handler403 = Views.permission_denied
+handler404 = Views.page_not_found
+handler500 = Views.server_error
