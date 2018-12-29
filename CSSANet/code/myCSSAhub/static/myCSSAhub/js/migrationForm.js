96b5c1d1463a5251d9371241709a5d8d478a71db
Caitlin <icesymeng@gmail.com>
2018-12-29 21:03:36 +1100

    confirmation page update

--- CSSANet/code/myCSSAhub/static/myCSSAhub/js/migrationForm.js
+++ CSSANet/code/myCSSAhub/static/myCSSAhub/js/migrationForm.js
@@ -14,3 +14,33 @@ $(function() {
     format: "L"
   });
 });
+
+$(".nextstep").click(function(){
+  if ($("#msform").parsley().isValid({group:"block1", force:false})){
+    console.log("Form Validation Complete")
+    var formData = new FormData($('#msform').get(0));
+    console.log(formData)
+    $.ajax({
+      type: "POST",
+      url: "/hub/migration/",
+      data: formData,
+      processData: false,
+      contentType: false,
+      async: false,
+      cache: false,
+      dataType: "json",
+      success: function (data) {
+        console.log(data)
+        href="/hub/regform/"+ data.migrationId +"/";
+        $("#completeMigration").attr("href",href)
+      },
+      error : function(xhr,errmsg,err) {
+        $('#ajax-errmsg').html('<div class="alert alert-dismissible alert-warning fade show" role="alert">Oops! We have encountered an error: '+errmsg+
+          " <a href='#' class='close'>&times;</a></div>"); // add the error to the dom
+          console.log(xhr.status + ": " + xhr.responseText); // provide a bit more info about the error to the console
+        }
+      });
+    } else {
+      $("#msform").parsley().whenValidate({group:"block1", force:false})
+    }
+});
