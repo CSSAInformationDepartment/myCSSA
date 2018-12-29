4e26b7c988327709bbbb9aad69a97c3ca9c81775
Shouyin <7000kl@163.com>
2018-12-29 18:17:32 +0800

    Update models.py

--- CSSANet/code/BlogAPI/models.py
+++ CSSANet/code/BlogAPI/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import sys
+sys.path.append("..")
+
+from UserAuthAPI.models import User
 
 # Create your models here.
+
+# 新闻本身
+class Blog (models.Model):
+    blogId = models.AutoField(primary_key = True)
+    blogTitle = models.CharField(max_length = 100)
+    blogMainContent = models.TextField()
+
+    # 阅读量
+    blogReads = models.IntegerField()
+
+class BlogWrittenBy(models.Model):
+    blogCreatedId = models.AutoField(primary_key = True)
+    blogId = models.ForeignKey(Blog, on_delete = models.CASCADE)
+    userId = models.ForeignKey(User, on_delete = models.DO_NOTHING)
+    writtenDate = models.DateTimeField(auto_now_add=True)
+
