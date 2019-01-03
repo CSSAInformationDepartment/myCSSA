83fa9952c8d9cdfa2473c36fdbb5f1bc13c63635
Le (Josh). Lu <joshlubox@gmail.com>
2019-01-03 14:40:12 +1100

    add major picker

---
+++ CSSANet/code/myCSSAhub/static/myCSSAhub/js/unimelb-major-picker.js
@@ -0,0 +1,73 @@
+  [{
+    "name": "Agriculture, Food science",
+    "abbreviation": "Agriculture, Food science"
+  },
+  {
+    "name": "Animal and veterinary",
+    "abbreviation": "Animal and veterinary"
+  },
+  {
+    "name": "Architecture, Building and planning",
+    "abbreviation": "Architecture, Building and planning"
+  },
+  {
+    "name": "Accounting, Finance and Actuarial",
+    "abbreviation": "Accounting, Finance and Actuarial"
+  },
+  {
+    "name": "Commerce, economics and management",
+    "abbreviation": "Commerce, economics and management"
+  },
+  {
+    "name": "Computing and Information",
+    "abbreviation": "Computing and Information"
+  },
+  {
+    "name": "Education, teaching and training",
+    "abbreviation": "Education, teaching and training"
+  },
+  {
+    "name": "Environment and natural resources",
+    "abbreviation": "Environment and natural resources"
+  },
+  {
+    "name": "Historical and philosophical Studies",
+    "abbreviation": "Historical and philosophical Studies"
+  },
+  {
+    "name": "Political and social studies",
+    "abbreviation": "Political and social studies"
+  },
+  {
+    "name": "Language, literature and cultural studies",
+    "abbreviation": "Language, literature and cultural studies"
+  },
+  {
+    "name": "Law, legal studies and criminology",
+    "abbreviation": "Law, legal studies and criminology"
+  },
+  {
+    "name": "Medicine, dentistry, health and welfare",
+    "abbreviation": "Medicine, dentistry, health and welfare"
+  },
+  {
+    "name": "Multidisciplinary",
+    "abbreviation": "Multidisciplinary"
+  },
+  {
+    "name": "Music",
+    "abbreviation": "Music"
+  },
+  {
+    "name": "Performing and visual arts",
+    "abbreviation": "Performing and visual arts"
+  },
+  {
+    "name": "Theatre, Film and television",
+    "abbreviation": "Theatre, Film and television"
+  },
+  {
+    "name": "Sciences and mathematics",
+    "abbreviation": "Sciences and mathematics"
+  }
+]
