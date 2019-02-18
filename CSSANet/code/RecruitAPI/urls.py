a9c0557cf2787a1976bbca58736387696c3f0df1
Le (Josh). Lu <joshlubox@gmail.com>
2019-02-18 11:21:11 +1100

    update job list view

--- CSSANet/code/RecruitAPI/urls.py
+++ CSSANet/code/RecruitAPI/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, re_path, include
-from .views import JobListJsonView
+from .views import *
 
 app_name = 'RecruitAPI'
 
 urlpatterns = [
-    path('list/', JobListJsonView.as_view(), name="job_list")
+    path('list/', JobListView.as_view(), name="job_list")
+]
+
+urlpatterns += [
+    path('ajax/list/', JobListJsonView.as_view(), name="job_jsonlist")
 ]
--- CSSANet/code/RecruitAPI/views.py
+++ CSSANet/code/RecruitAPI/views.py
@@ -8,32 +8,53 @@ from django.views import View
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django_datatables_view.base_datatable_view import BaseDatatableView
 
+from django.utils.html import escape
+
+
 
 from .forms import AddJobForm
+class JobListView(LoginRequiredMixin, PermissionRequiredMixin, View):
+    login_url = '/hub/login/'
+    permission_required = ('RecruitAPI.change_joblist',)
+    model = JobList
+    template_name='RecruitAPI/job_list.html'
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
 
 # Create your views here.
 class JobListJsonView(LoginRequiredMixin, PermissionRequiredMixin, BaseDatatableView):
     login_url = '/hub/login/'
-    permission_required = ('RecruitAPI.view_joblist',)
+    permission_required = ('RecruitAPI.change_joblist',)
     model = JobList
 
     # define the columns that will be returned
-    columns = ['timeOfCreate', 'jobName', 'dept', 'dueDate']
+    columns = ['jobName', 'dept.deptTitle','timeOfCreate',  'dueDate']
     order_columns = columns
 
     max_display_length = 500
 
+    def render_column(self, row, column):
+        # Customer HTML column rendering
+        if (column == 'timeOfCreate'):
+            return escape(row.timeOfCreate.strftime('%Y/%m/%d %H:%M:%S'))
+        elif (column == 'dueDate'):
+            return escape(row.dueDate.strftime('%Y/%m/%d %H:%M:%S'))
+        else:
+            return super(JobListJsonView, self).render_column(row, column)
+
     def get_initial_queryset(self):
         if not self.model:
             raise NotImplementedError("Need to provide a model or implement get_initial_queryset!")
-        return self.model.objects.all().order_by('-time')
+        return self.model.objects.filter(disabled=False).order_by('-timeOfCreate')
 
     def filter_queryset(self, qs):
         # DO NOT CHANGE THIS LINE
         search = self.request.GET.get('search[value]', None)
 
         if search:
-            qs = qs.filter(jobName__istartswith=search)
+            qs = qs.filter(Q(jobName__istartswith=search))
         return qs
 
 class AddJobView(PermissionRequiredMixin, CreateView):
