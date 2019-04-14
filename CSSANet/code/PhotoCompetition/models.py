9076ced1e535be1e4319f4f6e8c8e828a5027f90
Mia <1026280344@qq.com>
2019-04-14 13:24:22 +1000

    Create model of filessubmission

--- CSSANet/code/PhotoCompetition/models.py
+++ CSSANet/code/PhotoCompetition/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from UserAuthAPI import models as adminModel
 
 # Create your models here.
+class FilesSubmission(models.Model):
+    id = models.AutoField(primary_key = True, editable = False)
+    submission_id = models.ForeignKey(submission.Id, on_delete = models.Do_NOTHING)
+    submission_user=models.ForeignKey(adminModel.User, on_delete=models.DO_NOTHING)
+    upload_photo = models.FileField(default=None, null=True, blank=True, upload_to='competition/competitionPics')
+    Description = models.CharFiled(max_length = 250)
