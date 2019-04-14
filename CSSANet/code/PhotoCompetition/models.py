cfee36aca6f9d737a639647b4e5b7d8b57e594bd
Caitlin Jiang <icesymeng@gmail.com>
2019-04-14 13:26:37 +1000

    Update models.py

--- CSSANet/code/PhotoCompetition/models.py
+++ CSSANet/code/PhotoCompetition/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
+import uuid
+
+from UserAuthAPI import models as adminModel
 
 # Create your models here.
+class Submission(models.Models):
+    submissionId=models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    submissionUserId=models.ForeignKey(adminModel.User, on_delete=models.DO_NOTHING)
+    submissionTime=models.DateTimeField(auto_now_add=True)
+    DeviceChoice=(
+        ('MobilePhone','手机'),
+        ('Camera','相机'),
+    )
+    CategoryChoice=(
+        ('Nature','风景'),
+        ('Culture','人文')，
+    )
+    deviceType=models.CharField(verbose_name=_("设备"),choices=DeviceChoice, max_length=30, default="手机", null=True)
+    categoryType=models.CharField(verbose_name=_("类别"), choices=CategoryChoice, max_length=30, default="风景", null=True)
+    
