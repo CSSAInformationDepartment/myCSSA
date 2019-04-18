cba632a0aa289ace910b189d7afb68bae339d658
Le (Josh). Lu <joshlubox@gmail.com>
2019-04-18 20:46:35 +1000

    init views.py

--- CSSANet/code/PhotoCompetition/views.py
+++ CSSANet/code/PhotoCompetition/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import Http404, HttpResponse, HttpResponseRedirect
+from django.views import View
+from django.views.generic import CreateView, ListView, DeleteView
 
 # Create your views here.
