3c8daa48db3f6736fa7c177c1603c67567183504
Le (Josh). Lu <joshlubox@gmail.com>
2019-04-19 17:25:01 +1000

    add form attributes

--- CSSANet/code/PhotoCompetition/forms.py
+++ CSSANet/code/PhotoCompetition/forms.py
@@ -1,2 +1,9 @@
 from django import forms
-
+from PhotoCompetition import models
+class CandidateSubmissionForm(forms.ModelForm):
+    '''
+    PhotoCompetition ModelForm Class - Candidates Submission
+    '''
+    class Meta:
+        model = models.Submission
+        fields =('submissionUserId', 'deviceType', 'categoryType', 'upload_photo', 'description')
