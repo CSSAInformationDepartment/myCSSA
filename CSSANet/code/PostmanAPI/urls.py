89a2e87aea79a583a99fb0f85f1fc9d3323e3cea
Le (Josh). Lu <joshlubox@gmail.com>
2019-04-30 16:33:44 +1000

    init project PostmanAPI

---
+++ CSSANet/code/PostmanAPI/urls.py
@@ -0,0 +1,13 @@
+from rest_framework import routers
+from .views import MailDraftViewSet, MailQueneViewSet
+from django.urls import path,include
+
+app_name = 'PostmanAPI'
+
+postman_router = routers.SimpleRouter()
+postman_router.register(r'draft', MailDraftViewSet)
+postman_router.register(r'quene', MailQueneViewSet)
+
+urlpatterns = [
+    path('api/', include(postman_router.urls))
+]
