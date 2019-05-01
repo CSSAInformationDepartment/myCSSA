32798f2a1864d54f9e2a12fda8b37ec9159194e5
Le (Josh). Lu <joshlubox@gmail.com>
2019-05-01 16:13:55 +1000

    Adding system version indicator

--- CSSANet/code/CSSANet/settings/base.py
+++ CSSANet/code/CSSANet/settings/base.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     ## Server add-in
     'gunicorn',
     'widget_tweaks',
+    'django_filters',
     'storages',
     'redis',
     
@@ -101,7 +102,10 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 
