5b2ebb0e40ed3aa0a1cd72b1d54758b8efe0669a
Le (Josh). Lu <joshlubox@gmail.com>
2019-05-02 10:53:17 +1000

    Adding views doc

--- CSSANet/code/PostmanAPI/views.py
+++ CSSANet/code/PostmanAPI/views.py
@@ -4,11 +4,22 @@ from rest_framework import viewsets,generics,mixins
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated, BasePermission, DjangoModelPermissionsOrAnonReadOnly
 
+from django.utils.html import html_safe
+
 from . import models as PostmanModels
 from . import serializers as PostmanSerializers
 # Create your views here.
 
-class MailDraftViewSet(viewsets.ModelViewSet):
+class MailDraftViewSet(viewsets.ReadOnlyModelViewSet):
+    '''
+    Provide read-only viewset to inspect saved mail drafts
+    
+    URL Endpoints:
+        - List: ./postman/api/draft/
+        - Detail: ./postman/api/draft/<id>/
+
+    Author: Le(Josh). Lu
+    '''
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated,)
     queryset = PostmanModels.MailDraft.objects.all()
@@ -19,9 +30,31 @@ class MailDraftViewSet(viewsets.ModelViewSet):
 
 
 class MailQueneViewSet(viewsets.ModelViewSet):
+    '''
+    Provide read-only viewset to inspect mail quene
+    
+    URL Endpoints:
+        - List: ./postman/api/quene/
+        - Detail: ./postman/api/quene/<id>/
+
+    Author: Le(Josh). Lu
+    '''
     authentication_classes = (SessionAuthentication, TokenAuthentication)
     permission_classes = (IsAuthenticated, )
     queryset= PostmanModels.MailQuene.objects.all()
     serializer_class = PostmanSerializers.MailQueneSerializers
 
+class CreateMailDraftAPIView(generics.CreateAPIView):
+    '''
+    Provide read-only viewset to inspect saved mail drafts
     
+    URL Endpoints:
+        - List: ./postman/api/draft/
+        - Detail: ./postman/api/draft/<id>/
+
+    Author: Le(Josh). Lu
+    '''
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated, )
+    serializer_class = PostmanSerializers.MailDraftSerializers
+
