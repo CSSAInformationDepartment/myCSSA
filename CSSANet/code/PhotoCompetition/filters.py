fcabd53185aa5a4182a45542a8f0e993c927ea81
Le (Josh). Lu <joshlubox@gmail.com>
2019-05-09 16:31:04 +1000

    add submission selection filter

---
+++ CSSANet/code/PhotoCompetition/filters.py
@@ -0,0 +1,17 @@
+from django_filters import filters, FilterSet
+from django.utils.translation import ugettext_lazy as _
+from . import  models
+class SubmissionFilter(FilterSet):
+    SHORTLIST = (
+        ('not-shortlisted', _("未入选")),
+        ('shortlisted', _("已入选")),
+    )
+    first_name = filters.CharFilter(field_name='submissionUserId__firstNameEN', lookup_expr='istartswith', label=_("英文名"))
+    last_name = filters.CharFilter(field_name='submissionUserId__lastNameEN', lookup_expr='istartswith', label=_("英文姓"))
+    device_type = filters.ChoiceFilter(field_name='deviceType', choices=models.Submission.DEVICE_CHOICE, label=_("使用设备"))
+    category_type = filters.ChoiceFilter(field_name='categoryType', choices=models.Submission.CATEGORY_CHOICE, label=_("题材类型"))
+    shortlist_status = filters.ChoiceFilter(choices=SHORTLIST, label=_("是否入选"), method ="pesudo_filter")
+
+    def pesudo_filter(self, queryset, name, value):
+        return queryset
+
