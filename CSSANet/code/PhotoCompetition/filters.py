4b615fd2531dbb38d9597859e77d50881bb22799
Caitlin Jiang <icesymeng@gmail.com>
2019-08-21 22:48:27 +1000

    photo competition bug fixed

--- CSSANet/code/PhotoCompetition/filters.py
+++ CSSANet/code/PhotoCompetition/filters.py
@@ -20,5 +20,3 @@ class DisplaySubmissionFilter(FilterSet):
     device_type = filters.ChoiceFilter(field_name='deviceType', choices=models.Submission.DEVICE_CHOICE, label=_("使用设备"))
     category_type = filters.ChoiceFilter(field_name='categoryType', choices=models.Submission.CATEGORY_CHOICE, label=_("题材类型"))
     theme_type=filters.ChoiceFilter(field_name='themeType', choices=models.Submission.THEME_CHOICE, label=('主题类型'))
-    shortlist_status = filters.ChoiceFilter(field_name='shortlistType',choices=SHORTLIST, label=_("是否入选"), method ="pesudo_filter")
-
