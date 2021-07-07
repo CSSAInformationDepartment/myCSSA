5ec3e95e0b98a4933c137055a9cd6ef06d37228c
Liu233w <gh@liu233w.com>
2021-07-07 17:19:34 +1000

    Rename Forum to Community

---
+++ src/CommunityAPI/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CommunityAPIConfig(AppConfig):
+    name = 'CommunityAPI'
--- src/ForumAPI/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ForumapiConfig(AppConfig):
-    name = 'ForumAPI'
