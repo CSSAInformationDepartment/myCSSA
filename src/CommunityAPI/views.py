08f6293a055d67fba133cb71fe167505c235a775
Liu233w <gh@liu233w.com>
2021-07-08 15:46:59 +1000

    Community: add Tag serializer and views

--- src/CommunityAPI/views.py
+++ src/CommunityAPI/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import viewsets
+from rest_framework import permissions
+from . import serializers
+from .models import Tag
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    APIs to allow tags being read
+    """
+    queryset = Tag.objects.all().order_by('id')
+    serializer_class = serializers.TagSerializer
+    permission_classes = []
