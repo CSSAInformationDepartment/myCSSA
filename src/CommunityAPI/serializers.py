297d35ed330f5bde3eb60f5e9cf004c71cf68c98
Liu233w <gh@liu233w.com>
2021-07-14 01:21:37 +1000

    Add post read view set
    + can read one post or a post list
    + OpenAPI completable
    * functions are not tested yet

--- src/CommunityAPI/serializers.py
+++ src/CommunityAPI/serializers.py
@@ -1,8 +1,52 @@
 from rest_framework import serializers
 
+from UserAuthAPI.models import UserProfile
+
 from . import models
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Tag
         fields = ['id', 'title']
+
+class ContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Content
+        fields = ['title', 'text', 'imageUrls', 'editedTime']
+
+def resolve_username(profile: UserProfile) -> str:
+    # 暂时用用户的全名来当作用户名
+    return profile['lastNameCN'] + profile['firstNameCN']
+
+class PostReadSerializer(serializers.ModelSerializer):
+    """
+    只用来处理文章的读取
+    """
+
+    SUMMARY_TEXT_LENGTH = 50 # 25个汉字
+
+    content = ContentSerializer(read_only=True)
+
+    createdBy = serializers.CharField()
+
+    class Meta:
+        model = models.Post
+        fields = ['id', 'tagId', 'createTime', 'viewCount', 
+            # 正常情况下我们不需要再声明下面两个field，但是不这么搞的话 drf_yasg 会报错
+            'content', 'createdBy']
+
+    def to_representation(self, instance: models.Post):
+        repr = super().to_representation(instance)
+
+        contentModel = models.Content.objects.filter(postId=instance['id']).order_by('-editTime')
+        repr['content'] = self.content.to_representation(contentModel)
+
+        # 如果是读取文章列表，只保留正文的前25个汉字
+        if not self.content['view'].detail:
+            repr['content']['text'] = repr['content']['text'][:self.SUMMARY_TEXT_LENGTH]
+
+
+        userProfile = UserProfile.objects.get(pk=instance['createdBy'])
+        repr['createdBy'] = resolve_username(userProfile)
+
+        return repr
--- src/CommunityAPI/views.py
+++ src/CommunityAPI/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import permissions
 from . import serializers
-from .models import Tag
+from .models import Post, Tag
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
     """
@@ -14,3 +14,26 @@ class TagViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Tag.objects.all().order_by('id')
     serializer_class = serializers.TagSerializer
     permission_classes = [permissions.AllowAny]
+
+class PostReadViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    读取帖子
+    """
+
+    serializer_class = serializers.PostReadSerializer
+    permission_classes = [permissions.AllowAny]
+    
+    def get_queryset(self):
+        query = Post.objects.filter(
+            censored=False, 
+            deleted=False, 
+            replyToId=None, 
+            replyToComment=None,
+            )
+
+        if not self.request.user:
+            query = query.filter(viewableToGuest=True)
+
+        # 如果想要这个功能的话，可以在这里让管理员能看见被屏蔽和删除的文章
+
+        return query.order_by('-createTime')
