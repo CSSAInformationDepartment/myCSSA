f9405603e055d64851240cab7f8fb55ea920a112
Tianzhi Li <869909541@qq.com>
2021-07-24 15:56:12 +0800

    add admin Model

--- src/CommunityAPI/admin.py
+++ src/CommunityAPI/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from CommunityAPI.models import Tag
+from CommunityAPI.models import Tag, Post, Report
 
 # Register your models here.
 
 admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Report)
