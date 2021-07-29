78e6d9e01658796cb3403721d668b2a322169df9
Liu233w <gh@liu233w.com>
2021-07-30 00:51:06 +1000

    CommunityAPI-hub: 给 show_post 添加一些字段

--- src/CommunityAPI/hub_views.py
+++ src/CommunityAPI/hub_views.py
@@ -7,6 +7,11 @@ from rest_framework.generics import get_object_or_404
 from CommunityAPI.models import Post
 from .serializers import resolve_post_content
 
+def pk(instance):
+    if not instance:
+        return ''
+    else:
+        return instance.pk
 
 class ShowPostView(LoginRequiredMixin, TemplateView):
     login_url = '/hub/login/'
@@ -15,12 +20,26 @@ class ShowPostView(LoginRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
-        post = Post.objects.filter(id=self.request.GET.get('id', 0)).first()
+        id = self.request.GET.get('id', 0)
+        post: Post = Post.objects.filter(id=id).first()
         if post:
             return {
                 **context,
                 'post': post,
                 'content': resolve_post_content(post),
+                'post_fields': [
+                    ('创建时间', post.createTime),
+                    ('创建者', post.createdBy.pk),
+                    ('Tag', post.tag.title if post.tag else ''),
+                    ('未登录用户是否可见', post.viewableToGuest),
+                    ('被删除', post.deleted),
+                    ('被谁删除', pk(post.deletedBy)),
+                    ('被屏蔽', post.censored),
+                    ('被谁屏蔽', pk(post.censoredBy)),
+                ]
             }
         else:
-            return context
+            return {
+                **context,
+                'id': id,
+            }
