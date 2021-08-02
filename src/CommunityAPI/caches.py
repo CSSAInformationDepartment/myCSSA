930847b919f758dfd0604e6e86504b0c016bbc51
Liu233w <gh@liu233w.com>
2021-08-02 12:24:25 +1000

    Replace django cache with cachetools
    To use explicit memory caching

--- src/CommunityAPI/caches.py
+++ src/CommunityAPI/caches.py
@@ -1,18 +1,11 @@
-from django.core.cache import cache
+from cachetools import cached, TTLCache
 
 from CommunityAPI import models
 
 
+@cached(cache=TTLCache(maxsize=1024*1024, ttl=60*10)) # 1M, 10min
 def get_favourite_count_for_post(post_id):
     """
     从本地内存中获取缓存的收藏数量。默认缓存 10 分钟。
     """
-    key = 'favourite-count%d' % post_id
-
-    if cache.has_key(key):
-        return cache.get(key)
-
-    else:
-        count = models.FavouritePost.objects.filter(post_id=post_id).count()
-        cache.set(key, count, 600)
-        return count
+    return models.FavouritePost.objects.filter(post_id=post_id).count()
