69cd5125dfee6017857ab92fa16657e53070d445
czx20010217 <82380384+czx20010217@users.noreply.github.com>
2021-08-09 21:52:39 +0800

    Update permissions.py
    用has_perm需要写上app的名字作为前缀

--- src/CommunityAPI/permissions.py
+++ src/CommunityAPI/permissions.py
@@ -1,15 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
 
-
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return request.user and obj.createdBy_id == request.user.id
 
 class CanHandleReport(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_perm('can_handle_report')
+        return request.user.has_perm('CommunityAPI.can_handle_report')
 
 class CanCensorPost(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_perm('censor_post')
+        return request.user.has_perm('CommunityAPI.censor_post')
