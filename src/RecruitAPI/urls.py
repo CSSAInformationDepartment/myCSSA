f762bd4a63ce5ea4d28f6308806c60914dd1b8ba
czx20010217 <82380384+czx20010217@users.noreply.github.com>
2021-12-25 14:12:26 +0800

    新增检查各部门是否正在招新接口
    api: GET /hub/recruit/joblist_availability_check/

--- src/RecruitAPI/urls.py
+++ src/RecruitAPI/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('resume/', ResumeListView.as_view(), name="resume_list"),
     path('resume/<str:id>/detail/', ResumeDetailView.as_view(), name="resume_detail"),
     path('resume/<str:id>/add_to_interview/', AddInterviewView.as_view(), name="add_to_interview"),
+    path('joblist_availability_check/', JoblistAvailabilityViewSet.as_view({'get': 'list'}), name='joblist_availability_check')
 ]
 
 urlpatterns += [
