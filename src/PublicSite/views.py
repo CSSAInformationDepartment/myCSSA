9ab7f3bb6c4104c6697df8c9a0f60577cef7d139
Chenyang Dong <zjgdcy@gmail.com>
2023-03-24 17:54:11 +1100

    rollback the ordering of future events

--- src/PublicSite/views.py
+++ src/PublicSite/views.py
@@ -54,7 +54,7 @@ def index(request):
     '''
     now_time = timezone.now()
     eventsPast=eventModels.Event.objects.filter(eventActualStTime__lt=now_time).order_by("-eventActualStTime")
-    eventsFuture=eventModels.Event.objects.filter(eventActualStTime__gt=now_time).order_by("-eventActualStTime")
+    eventsFuture=eventModels.Event.objects.filter(eventActualStTime__gt=now_time).order_by("eventActualStTime")
     header_caro = models.HomepageCarousels.objects.all()
     return render(request, 'PublicSite/index.html',
         {'now_time':now_time,'eventsPast':eventsPast,'eventsFuture':eventsFuture,
@@ -173,7 +173,7 @@ class EventsListView(View):
     def get(self, request, *args, **kwargs):
         timezone.activate('Australia/Melbourne')
         now_time = timezone.now()
-        eventsFuture=eventModels.Event.objects.filter(eventActualStTime__gt=now_time).order_by("-eventActualStTime")
+        eventsFuture=eventModels.Event.objects.filter(eventActualStTime__gt=now_time).order_by("eventActualStTime")
         eventsPast=eventModels.Event.objects.filter(eventActualStTime__lt=now_time).order_by("-eventActualStTime")
         return render(request, self.template_name, {'eventsFuture':eventsFuture, 'now_time':now_time,'events':self.events, 'eventsPast':eventsPast})
 
