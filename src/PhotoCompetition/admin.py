0f6c6f947bf454fe769911037a202cf79bf88a76
Chenyang Dong <zjgdcy@gmail.com>
2023-08-17 21:37:50 +1000

    style: Format and lint files - PhotoCompetition
    Using autopep8, djlint, and Prettier

--- src/PhotoCompetition/admin.py
+++ src/PhotoCompetition/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . import models
-# Register your models here.
 
+from . import models
+
+# Register your models here.
 
 
 admin.site.register(models.Submission)
--- src/PhotoCompetition/filters.py
+++ src/PhotoCompetition/filters.py
@@ -1,22 +1,35 @@
-from django_filters import filters, FilterSet
 from django.utils.translation import ugettext_lazy as _
-from . import  models
+from django_filters import FilterSet, filters
+
+from . import models
+
+
 class SubmissionFilter(FilterSet):
     SHORTLIST = (
         ('not-shortlisted', _("未入选")),
         ('shortlisted', _("已入选")),
     )
-    first_name = filters.CharFilter(field_name='submissionUserId__firstNameEN', lookup_expr='istartswith', label=_("英文名"))
-    last_name = filters.CharFilter(field_name='submissionUserId__lastNameEN', lookup_expr='istartswith', label=_("英文姓"))
-    device_type = filters.ChoiceFilter(field_name='deviceType', choices=models.Submission.DEVICE_CHOICE, label=_("使用设备"))
-    category_type = filters.ChoiceFilter(field_name='categoryType', choices=models.Submission.CATEGORY_CHOICE, label=_("题材类型"))
-    theme_type=filters.ChoiceFilter(field_name='themeType', choices=models.Submission.THEME_CHOICE, label=('主题类型'))
-    shortlist_status = filters.ChoiceFilter(field_name='shortlistType',choices=SHORTLIST, label=_("是否入选"), method ="pesudo_filter")
+    first_name = filters.CharFilter(
+        field_name='submissionUserId__firstNameEN', lookup_expr='istartswith', label=_("英文名"))
+    last_name = filters.CharFilter(
+        field_name='submissionUserId__lastNameEN', lookup_expr='istartswith', label=_("英文姓"))
+    device_type = filters.ChoiceFilter(
+        field_name='deviceType', choices=models.Submission.DEVICE_CHOICE, label=_("使用设备"))
+    category_type = filters.ChoiceFilter(
+        field_name='categoryType', choices=models.Submission.CATEGORY_CHOICE, label=_("题材类型"))
+    theme_type = filters.ChoiceFilter(
+        field_name='themeType', choices=models.Submission.THEME_CHOICE, label=('主题类型'))
+    shortlist_status = filters.ChoiceFilter(
+        field_name='shortlistType', choices=SHORTLIST, label=_("是否入选"), method="pesudo_filter")
 
     def pesudo_filter(self, queryset, name, value):
         return queryset
 
+
 class DisplaySubmissionFilter(FilterSet):
-    device_type = filters.ChoiceFilter(field_name='deviceType', choices=models.Submission.DEVICE_CHOICE, label=_("使用设备"))
-    category_type = filters.ChoiceFilter(field_name='categoryType', choices=models.Submission.CATEGORY_CHOICE, label=_("题材类型"))
-    theme_type=filters.ChoiceFilter(field_name='themeType', choices=models.Submission.THEME_CHOICE, label=('主题类型'))
+    device_type = filters.ChoiceFilter(
+        field_name='deviceType', choices=models.Submission.DEVICE_CHOICE, label=_("使用设备"))
+    category_type = filters.ChoiceFilter(
+        field_name='categoryType', choices=models.Submission.CATEGORY_CHOICE, label=_("题材类型"))
+    theme_type = filters.ChoiceFilter(
+        field_name='themeType', choices=models.Submission.THEME_CHOICE, label=('主题类型'))
