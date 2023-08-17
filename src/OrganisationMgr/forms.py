e729a8776ae2f778f396664bc2f2002babac0b56
Chenyang Dong <zjgdcy@gmail.com>
2023-08-17 21:55:13 +1000

    style: Format and lint files - OrganisationMgr
    using autopep8, djlint, and Prettier

--- src/OrganisationMgr/forms.py
+++ src/OrganisationMgr/forms.py
@@ -1,20 +1,17 @@
-from UserAuthAPI.models import User, UserProfile, CSSACommitteProfile
-from LegacyDataAPI.models import LegacyUsers
-
 from django import forms
 from django.forms import ValidationError
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
-
-
+from LegacyDataAPI.models import LegacyUsers
+from UserAuthAPI.models import CSSACommitteProfile, User, UserProfile
 
 
 class BindingMembershipCardForm(forms.ModelForm):
-    
+
     class Meta:
         model = UserProfile
         fields = ('user', 'membershipId')
-        help_texts ={
+        help_texts = {
             'membershipId': _("会员卡号最多10位"),
         }
         widgets = {
@@ -26,50 +23,62 @@ class BindingMembershipCardForm(forms.ModelForm):
         super().clean()
         membership_Id = self.cleaned_data.get('membershipId')
 
-        prev_member = UserProfile.objects.filter(membershipId=membership_Id).first()
-        legacy_member = LegacyUsers.objects.filter(membershipId=membership_Id).first()
+        prev_member = UserProfile.objects.filter(
+            membershipId=membership_Id).first()
+        legacy_member = LegacyUsers.objects.filter(
+            membershipId=membership_Id).first()
 
         if prev_member or legacy_member:
-            raise ValidationError(_(mark_safe('<li>该会员卡状态异常，请更换</li>')), code='abnormal card')
+            raise ValidationError(
+                _(mark_safe('<li>该会员卡状态异常，请更换</li>')), code='abnormal card')
 
 
 class UserProfileEditForm(forms.ModelForm):
     user = forms.CharField(disabled=True)
+
     class Meta:
-        
+
         model = UserProfile
-        exclude = ('avatar', 'infocardBg', 'identiyConfirmed', 'isValid', 'joinDate', 'dateOfBirth', 'membershipId')
+        exclude = ('avatar', 'infocardBg', 'identiyConfirmed',
+                   'isValid', 'joinDate', 'dateOfBirth', 'membershipId')
 
     def clean(self, *args, **kwargs):
         super().clean()
         student_id = self.cleaned_data.get('studentId')
         user_id = self.cleaned_data.get('user')
 
-        duplicated_student_id = UserProfile.objects.exclude(user__pk=user_id).filter(studentId=student_id).first()
+        duplicated_student_id = UserProfile.objects.exclude(
+            user__pk=user_id).filter(studentId=student_id).first()
         if duplicated_student_id:
-            raise ValidationError(_(mark_safe('<li>该学生卡号已被占用</li>')), code='Duplicated Student Id')
+            raise ValidationError(
+                _(mark_safe('<li>该学生卡号已被占用</li>')), code='Duplicated Student Id')
+
 
 class UserEditForm(forms.ModelForm):
     email = forms.CharField(disabled=True)
 
     class Meta:
         model = User
-        fields = ('email','telNumber')
+        fields = ('email', 'telNumber')
 
     def clean(self, *args, **kwargs):
         super().clean()
         email = self.cleaned_data.get('email')
         telNumber = self.cleaned_data.get('telNumber')
 
-        duplicated_telNumber = User.objects.exclude(email=email).filter(telNumber=telNumber).first()
+        duplicated_telNumber = User.objects.exclude(
+            email=email).filter(telNumber=telNumber).first()
         if duplicated_telNumber:
-            raise ValidationError(_(mark_safe('<li>该手机号已被他人注册</li>')), code='Duplicated tel number')
+            raise ValidationError(
+                _(mark_safe('<li>该手机号已被他人注册</li>')), code='Duplicated tel number')
+
 
 class AssignNewComitteeForm(forms.ModelForm):
     is_active = forms.BooleanField(initial=True, widget=forms.HiddenInput())
+
     class Meta:
         model = CSSACommitteProfile
-        fields = ('member','Department','role','is_active',)
+        fields = ('member', 'Department', 'role', 'is_active',)
         widgets = {
             'member': forms.HiddenInput,
         }
--- src/OrganisationMgr/urls.py
+++ src/OrganisationMgr/urls.py
@@ -1,23 +1,27 @@
 
-from django.urls import path, re_path, include
+from django.urls import path
+
 from OrganisationMgr import views
-
-
 
 app_name = "OrganisationMgr"
 urlpatterns = [
-    path('deptmgr/', views.DepartmentManagementView.as_view(), name="dept_management"),
-    path('deptmgr/committee/<str:id>/add/',views.AddNewCommitteView.as_view(), name="dept_add_committee"),
+    path('deptmgr/', views.DepartmentManagementView.as_view(),
+         name="dept_management"),
+    path('deptmgr/committee/<str:id>/add/',
+         views.AddNewCommitteView.as_view(), name="dept_add_committee"),
 
     path('member/', views.MemberListView.as_view(), name="member_list"),
-    path('member/<str:id>/update/', views.UserProfileEditView.as_view(), name="update_member"),
+    path('member/<str:id>/update/',
+         views.UserProfileEditView.as_view(), name="update_member"),
     path('new_member/', views.MemberSearchView.as_view(), name='new_member'),
-    path('new_member/<str:id>/detail/', views.MembershipActivationView.as_view(), name='new_member_activation'),
-    path('new_member/<str:id>/result/', views.ConfirmActivationView.as_view(), name='confirm_activation'),
+    path('new_member/<str:id>/detail/',
+         views.MembershipActivationView.as_view(), name='new_member_activation'),
+    path('new_member/<str:id>/result/',
+         views.ConfirmActivationView.as_view(), name='confirm_activation'),
 ]
 
 
-## Internal AJAX path
+# Internal AJAX path
 urlpatterns += [
-    
+
 ]
