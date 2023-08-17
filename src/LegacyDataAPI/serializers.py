b42aea67c9157fc400689f6e444140957fc62423
Chenyang Dong <zjgdcy@gmail.com>
2023-08-17 22:06:02 +1000

    style: Format and lint files - LegacyDataAPI

--- src/LegacyDataAPI/serializers.py
+++ src/LegacyDataAPI/serializers.py
@@ -24,29 +24,33 @@
 ###############################################################################
 
 from rest_framework import serializers
+
 from LegacyDataAPI import models
+
 
 class LegacyUserSerializers(serializers.ModelSerializer):
     class Meta:
-        fields = ('firstNameEN', 'lastNameEN', 'gender', 'dateOfBirth', 'studentId', 'membershipId', 'telNumber', 'email') 
+        fields = ('firstNameEN', 'lastNameEN', 'gender', 'dateOfBirth',
+                  'studentId', 'membershipId', 'telNumber', 'email')
         model = models.LegacyUsers
-        read_only_fields = ('firstNameEN', 'lastNameEN', 'gender', 'dateOfBirth', 'studentId', 'membershipId', 'telNumber', 'email')
+        read_only_fields = ('firstNameEN', 'lastNameEN', 'gender',
+                            'dateOfBirth', 'studentId', 'membershipId', 'telNumber', 'email')
+
 
 class LegitCheck(serializers.Serializer):
-    #A类记录字段
+    # A类记录字段
     membershipId = serializers.CharField(required=False, write_only=True)
     studentId = serializers.CharField(required=False, write_only=True)
     firstNameEN = serializers.CharField(required=False, write_only=True)
-    lastNameEN =  serializers.CharField(required=False, write_only=True)
-    dateOfBirth =  serializers.DateField(required=False, write_only=True)
+    lastNameEN = serializers.CharField(required=False, write_only=True)
+    dateOfBirth = serializers.DateField(required=False, write_only=True)
 
-    #B类记录
+    # B类记录
     telNumber = serializers.CharField(required=False, write_only=True)
     email = serializers.CharField(required=False, write_only=True)
 
-    #结果
+    # 结果
     outcome = serializers.CharField(required=False, write_only=True)
-
 
     class Meta:
         fields = '__all__'
