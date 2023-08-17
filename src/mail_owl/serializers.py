df12c96009c163e4b4182ba90906693aa362a32d
Chenyang Dong <zjgdcy@gmail.com>
2023-08-17 22:04:07 +1000

    style: Format and lint files - mail_owl
    Using autopep8, djlint, and Prettier

--- src/mail_owl/serializers.py
+++ src/mail_owl/serializers.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
+
 from . import models as PostmanModels
+
 
 class MailDraftSerializers(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source='author.id')
+
     class Meta:
         model = PostmanModels.MailDraft
         fields = '__all__'
-    
+
 
 class MailQueneSerializers(serializers.ModelSerializer):
     class Meta:
         model = PostmanModels.MailQuene
         fields = '__all__'
-
-    
