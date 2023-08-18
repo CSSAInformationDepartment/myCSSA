8b9dabf2533b0c8527d01e86523d2506ea071c5d
Chenyang Dong <zjgdcy@gmail.com>
2023-08-18 12:44:29 +1000

    style: Format and lint files - PublicSite

--- src/PublicSite/admin.py
+++ src/PublicSite/admin.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
+
 from PublicSite import models
+
 # Register your models here.
 
+
 class HomepageCarouselAdmin(admin.ModelAdmin):
-    list_display = ('id','url','filePath','header','description')
+    list_display = ('id', 'url', 'filePath', 'header', 'description')
     list_display_links = ('id',)
     search_fields = ('id',)
     list_per_page = 5
-
 
 
 admin.site.register(models.HTMLFields)
